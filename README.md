cc0e9e1e8e18266c9818a369925e73ba70915095
push0ebp <push0ebp@push0ebpui-MacBook-Pro.local>
2018-09-26 06:59:37 +0900

    Add api_deobfuscator.lua

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # api-deobfuscator
-Fix API against Themida API Redirection/Jump Trick with Cheat Engine Lua Script 
+Fix API against Themida `API Redirection/Jump Trick` with Cheat Engine Lua Script 
+
+
+## Usage
+`Cheat Engine` - `Attach Process` - `Memory View` - `Lua Engine (Ctrl+L)` - `Paste` - `Execute` 
+Then, Use your `API/IAT Fixer tool`.
+
+This tool is just API Jump Tricked deobfuscator.
+
+Enjoy.
+
+##### TODO
+`call dword ptr fs:[000000C0]`
+
+## Features
+![Startup before](https://raw.githubusercontent.com/push0ebp/api-deobfuscator/master/images/startup_before.png)
+![Startup after](https://raw.githubusercontent.com/push0ebp/api-deobfuscator/master/images/startup_after.png)
+
+![JmpTable before](https://raw.githubusercontent.com/push0ebp/api-deobfuscator/master/images/jmptable_before.png)
+![JmpTable after](https://raw.githubusercontent.com/push0ebp/api-deobfuscator/master/images/jmptable_after.png)
+
+
+## Result
+![Result](https://raw.githubusercontent.com/push0ebp/api-deobfuscator/master/images/result.png)
---
+++ api_deobfuscator.lua
@@ -0,0 +1,89 @@
+
+function disas(addr)
+  local disassStr = disassemble(addr)
+  local extraField, opcode, bytes, address = splitDisassembledString(disassStr)
+  return address, opcode
+end
+
+function getDestAddr(addr, jmp)
+  local address, opcode = disas(addr)
+  local destAddr = nil
+  if (jmp and string.match(opcode, '^j%a+%s+')) or
+    string.find(opcode, "call") then
+    local addr = string.match(opcode, '%s+(%x+)$')
+    if addr then
+      destAddr = tonumber(addr, 16)
+    end
+  end
+  return destAddr
+end
+
+  
+function follows(addr)
+  local CNT = 0x300
+  local pc = addr
+  for i = 0, CNT do
+    local destAddr = getDestAddr(pc, true)
+    if destAddr then
+      pc = destAddr
+    else 
+      pc = pc + getInstructionSize(pc)
+    end
+    if inSystemModule(pc) then
+      return pc
+    end
+  end
+  return nil
+end
+
+function getApiAddr(addr)
+  local apiAddr = follows(addr)
+  if apiAddr then
+    apiAddr = getNameFromAddress(apiAddr)
+    apiAddr = string.gsub(apiAddr, '%+(%x+)$', "")
+    apiAddr = getAddress(apiAddr)
+    return apiAddr
+  end
+  return nil
+end
+
+function fix_api(addr)
+  local funcAddr = getDestAddr(addr, true)
+  local apiAddr = getApiAddr(funcAddr)
+  if apiAddr then
+    local scriptStr = [==[
+        %x:
+        %s
+    ]==]  
+    local address, opcode = disas(addr)
+    local ins = string.match(opcode, '^%a+%s+')
+    local insStr = string.format("%s %x", ins, apiAddr)  
+    scriptStr = string.format(scriptStr, addr, insStr)
+    autoAssemble(scriptStr) 
+  end
+  return apiAddr
+end
+
+function fixs(from, to)
+  local pc = from
+  local cnt = 0
+  while pc < to do
+    local destAddr = getDestAddr(pc, true)
+    if destAddr and getAddressSafe(destAddr) and not inModule(destAddr) then
+      local apiAddr = fix_api(pc)
+      if apiAddr then 
+        cnt = cnt + 1
+        print(string.format("(%d) %x[%s] - %s", cnt, pc, getNameFromAddress(pc), getNameFromAddress(apiAddr)))
+      end
+    end
+    pc = pc + getInstructionSize(pc)
+  end
+  print("Finished")
+  return pc
+end
+
+local from = getAddress("PROCESS NAME") + 0x1000 -- modify base of your module code 
+local size = 0x30000 -- modify size of code
+local to = from + size
+fixs(from, to)
+print(string.format("From %x To %x", from, to))
